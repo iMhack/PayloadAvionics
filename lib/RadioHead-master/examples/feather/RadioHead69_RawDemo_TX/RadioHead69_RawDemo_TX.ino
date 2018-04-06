9cd36dd0fbd73092ef5052cad7eb9c7f0ff36962
Quentin Talon <quentin.talon@epfl.ch>
2018-04-06 16:40:32 +0200

    RF
    Essai du module RF. Ca marche pas là :-) Ni le petit receiver avec le Arduino Nano ni le Teensy.

--- lib/RadioHead-master/examples/feather/RadioHead69_RawDemo_TX/RadioHead69_RawDemo_TX.ino
+++ lib/RadioHead-master/examples/feather/RadioHead69_RawDemo_TX/RadioHead69_RawDemo_TX.ino
@@ -5,54 +5,80 @@
 // reliability, so you should only use RH_RF69  if you do not need the higher
 // level messaging abilities.
 // It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem
+// Demonstrates the use of AES encryption, setting the frequency and modem 
 // configuration
 
 #include <SPI.h>
 #include <RH_RF69.h>
-#include <RHSPIDriver.h>
-#include <RHGenericSPI.h>
 
 /************ Radio Setup ***************/
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF69_FREQ 433.0
+#define RF69_FREQ 915.0
 
-//* Teensy 3.x w/wing */
-#define RFM69_RST     24   // "A"
-#define RFM69_EN      8
-#define LED           2   // "A"
-#define RFM69_CS      9   // "B"
-#define RFM69_IRQ     29    // "C"
+#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
+  #define RFM69_CS      8
+  #define RFM69_INT     7
+  #define RFM69_RST     4
+  #define LED           13
+#endif
+
+#if defined(ARDUINO_SAMD_FEATHER_M0) // Feather M0 w/Radio
+  #define RFM69_CS      8
+  #define RFM69_INT     3
+  #define RFM69_RST     4
+  #define LED           13
+#endif
+
+#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+  #define RFM69_INT     3  // 
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
+  #define LED           13
+#endif
+
+#if defined(ESP8266)    // ESP8266 feather w/wing
+  #define RFM69_CS      2    // "E"
+  #define RFM69_IRQ     15   // "B"
+  #define RFM69_RST     16   // "D"
+  #define LED           0
+#endif
+
+#if defined(ESP32)    // ESP32 feather w/wing
+  #define RFM69_RST     13   // same as LED
+  #define RFM69_CS      33   // "B"
+  #define RFM69_INT     27   // "A"
+  #define LED           13
+#endif
+
+/* Teensy 3.x w/wing
+#define RFM69_RST     9   // "A"
+#define RFM69_CS      10   // "B"
+#define RFM69_IRQ     4    // "C"
 #define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ )
-//*/
+*/
+ 
+/* WICED Feather w/wing 
+#define RFM69_RST     PA4     // "A"
+#define RFM69_CS      PB4     // "B"
+#define RFM69_IRQ     PA15    // "C"
+#define RFM69_IRQN    RFM69_IRQ
+*/
 
 // Singleton instance of the radio driver
-RH_RF69 rf69(RFM69_CS, RFM69_IRQ);
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
-void Blink(byte PIN, byte DELAY_MS, byte loops) {
-  for (byte i=0; i<loops; i++)  {
-    digitalWrite(PIN,HIGH);
-    delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
-    delay(DELAY_MS);
-  }
-}
-
-
-void setup()
+void setup() 
 {
-  pinMode(LED, OUTPUT);
-  Blink(LED,50,2);
-  Serial.begin(9600);
-  while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
-  pinMode(RFM69_EN, OUTPUT);
+  Serial.begin(115200);
+  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+
+  pinMode(LED, OUTPUT);     
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
-  digitalWrite(RFM69_RST, HIGH);
-  Blink(LED,50,2);
+
   Serial.println("Feather RFM69 TX Test!");
   Serial.println();
 
@@ -61,12 +87,9 @@ void setup()
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
-  int tests = spiRead(RH_RF69_REG_10_VERSION);
-  Serial.println(tests);
+  
   if (!rf69.init()) {
     Serial.println("RFM69 radio init failed");
-
-    Blink(LED,50,5);
     while (1);
   }
   Serial.println("RFM69 radio init OK!");
@@ -84,11 +107,12 @@ void setup()
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
-
+  
   pinMode(LED, OUTPUT);
 
   Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
 }
+
 
 
 void loop() {
@@ -97,7 +121,7 @@ void loop() {
   char radiopacket[20] = "Hello World #";
   itoa(packetnum++, radiopacket+13, 10);
   Serial.print("Sending "); Serial.println(radiopacket);
-
+  
   // Send a message!
   rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
   rf69.waitPacketSent();
@@ -106,8 +130,8 @@ void loop() {
   uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
 
-  if (rf69.waitAvailableTimeout(500))  {
-    // Should be a reply message for us now
+  if (rf69.waitAvailableTimeout(500))  { 
+    // Should be a reply message for us now   
     if (rf69.recv(buf, &len)) {
       Serial.print("Got a reply: ");
       Serial.println((char*)buf);
@@ -117,5 +141,14 @@ void loop() {
     }
   } else {
     Serial.println("No reply, is another RFM69 listening?");
+  }
+}
+
+void Blink(byte PIN, byte DELAY_MS, byte loops) {
+  for (byte i=0; i<loops; i++)  {
+    digitalWrite(PIN,HIGH);
+    delay(DELAY_MS);
+    digitalWrite(PIN,LOW);
+    delay(DELAY_MS);
   }
 }
