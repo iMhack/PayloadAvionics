deed9ae4b201951277596c6d49e9a956ce89a5c7
qwetzal <35138217+qwetzal@users.noreply.github.com>
2018-06-03 22:55:22 +0200

    Automatic file generation
    The code has been partially cleaned, functions added
    
    Files are automatically generated during the setup
    
    Few more steps to get the required freq (minimize the constant time used)

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
 // and IntervalTimer
 #include <IntervalTimer.h>
 #include <SD.h>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
 
 #define BUF_SIZE 8 //256 for 2 adc, 512 for one (max), 8 for 8 channel 1 ADC and a larger ADC
 #define GLOBAL_BUF 4096 //32768 //size of the buffer that empties the adc buffer through an interrupt function
@@ -30,35 +36,36 @@ DMAChannel* dma1 = new DMAChannel(false);
 //ChannelsCfg order must be {CH1, CH2, CH3, CH0 }, adcbuffer output will be CH0, CH1, CH2, CH3
 //Order must be {Second . . . . . . . . First} no matter the number of channels used.
 const uint16_t ChannelsCfg_0 [] =  { 0x4E, 0x48, 0x49, 0x46, 0x47, 0x4F, 0x44, 0x45 };  //ADC0: CH0 ad6(A6), CH1 ad7(A7), CH2 ad15(A8), CH3 ad4(A9) works
-//const uint16_t ChannelsCfg_0 [] =  { 0x49, 0x4F, 0x46, 0x48 };  //ADC0: CH0 ad6(A6), CH1 ad7(A7), CH2 ad15(A8), CH3 ad4(A9)
-//const uint16_t ChannelsCfg_1 [] =  { 0x4E, 0x19, 0x1A, 0x45 };  //ADC1: CH0 ad4(A17), CH1 ad5(A16), CH2ad6(A18), CH3 ad7(A19)
-/*ADC0:
-A0=0x45
-A1=0x4E
-A2=0x48
-A3=0x49
-A6=0x46
-A7=0x47
-A8=0x4F
-A9=0x44
-*/
+
+/*ADC0: A0=0x45 A1=0x4E A2=0x48 A3=0x49 A6=0x46 A7=0x47A8=0x4F A9=0x44*/
+
 const int ledPin = 13;
 
-const int period0 = 1; // us
-const int periodFile = 10000000; // us, every 10 seconds a new file is opened
+const int flightLength = 15; //length of the flight in min
+
+const int period0 = 100; // us
+unsigned long filePeriod = 30000; //ms (compared to an elapsedMillis)
+unsigned long criticTime = 50; //to test
+
+const double nbFiles=floor((flightLength*60*1000)/filePeriod);
 
 //Initialize the buffers
 DMAMEM static volatile uint16_t __attribute__((aligned(BUF_SIZE+0))) adcbuffer[BUF_SIZE];
-static volatile uint16_t __attribute__((aligned(GLOBAL_BUF+0))) globalbuffer1[GLOBAL_BUF];
-static volatile uint16_t __attribute__((aligned(GLOBAL_BUF+0))) globalbuffer2[GLOBAL_BUF];
-const uint16_t initBuff = 50000;
+static  uint8_t __attribute__((aligned(GLOBAL_BUF+0))) copybuffer[GLOBAL_BUF]; //to transverse the data from the two other buffers
+static  uint8_t __attribute__((aligned(GLOBAL_BUF+0))) globalbuffer1[GLOBAL_BUF];
+static  uint8_t __attribute__((aligned(GLOBAL_BUF+0))) globalbuffer2[GLOBAL_BUF];
+const uint8_t initBuff = 255;
 volatile int pos=0; //current position within the global buffers
 volatile bool BUFF=false; //choose which buffer should be filled, which one should be emptied (false: fill 1, empty 2 and vv)
+int setupIdx[]={48,48};
+int fileIdx[]={48,48};
 
 volatile int d2_active;
 
 elapsedMillis debounce;
+elapsedMillis timeLog;
 elapsedMillis timecheck;
+elapsedMillis sinceFile; //allows the periodic creation of a new file
 elapsedMicros callbacktime;
 
 //void dma2_isr(void);
@@ -66,11 +73,33 @@ void dma0_isr(void);
 void d2_isr(void);
 void setup_adc() ;
 void setup_dma();
+void setup_files();
+void increment_str(int *idx, char* s);
+void increment_idx(int *idx);
 void callback(void);
 void fileManager(void);
 
+char str[10];
+
+/*
+str[9]=c;
+str[10]=c;
+str[11]=c;*/
+
 void setup() {
   // initialize the digital pin as an output.
+
+  str[0]='M';
+  str[1]='0';
+  str[2]='0';
+  str[3]='0';
+  str[4]='0';
+  str[5]='0';
+  str[6]='.';
+  str[7]='t';
+  str[8]='x';
+  str[9]='t';
+
   pinMode(ledPin, OUTPUT);
 
   d2_active = 0;
@@ -83,6 +112,8 @@ void setup() {
 
   delay(5000);
 
+  Serial.begin(9600);
+
   Serial.println("SD card config");
   if (!SD.begin(chipSelect))
   {
@@ -91,12 +122,16 @@ void setup() {
   // return; //this line makes it directly jumps to loop - use the boolean setupFail instead
   } Serial.println("Card initialized.");
 
-  Serial.begin(9600);
-
   // clear buffers (adc output should never go higher than 4095 in 12 bits config)
   for (int i = 0; i < BUF_SIZE; ++i){
       adcbuffer[i]=initBuff;
   }
+
+  delay(100);
+
+  setup_files(); //setup the directories and files that have to be created
+
+  delay(100);
 
   for (int i = 0; i < GLOBAL_BUF; ++i){
       globalbuffer1[i]=initBuff;
@@ -105,70 +140,62 @@ void setup() {
 
   setup_adc();
   setup_dma();
+
   startTimerValue0 = timer0.begin(callback, period0);
-  startTimerValue1 = timer1.begin(fileManager, periodFile);
+  //startTimerValue1 = timer1.begin(fileManager, periodFile);
+  //delay(1000);
+  Serial.println("Setup end !");
 }
+
+
 
 //uint16_t analog=0; //variable that will store locally the value of the buffer
 
 void loop() {
 
-  timecheck=0;
-  /*
-  File testFile = SD.open("testlog.txt", FILE_WRITE);
+  timecheck=0; //check the duration of the loop
 
-  // if the file is available, write to it:
+
+  File testFile = SD.open(str, FILE_WRITE);
+
   if (testFile) {
-    if (BUFF==false) {
-      BUFF=true; //switch to the second buffer before emptying the first
-      pos=0; //initialize back the position
-      for (int i=0;i<GLOBAL_BUF;i=i+8) {
-        if (globalbuffer2[i]!=initBuff) {
-          testFile.print(globalbuffer2[i]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+1]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+2]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+3]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+4]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+5]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+6]); testFile.print("  ");
-          testFile.println(globalbuffer2[i+7]);
-          if (timecheck>500) testFile.println("fuckup");
-          globalbuffer2[i]=initBuff;
+    std::copy(std::begin(globalbuffer1), std::end(globalbuffer1), std::begin(globalbuffer2));
+    pos=0; //initialize back the position
+    testFile.println(timeLog);
+    for (int k=0;k<400;k=k+BUF_SIZE) { //go through the buffer
+      if (globalbuffer2[k]!=initBuff) { //stop when you reach the last written line
+        for (int l=0;l<BUF_SIZE;l=l+1) { //go through each pin
+          //if (globalbuffer2[k+l]<20){ //only write if the buffer respects the threshold
+            testFile.print(globalbuffer2[k+l]); testFile.print("  ");
+          //}
         }
+        globalbuffer1[k]=initBuff;
       }
     }
-    else {
-      BUFF=false; //switch to the second buffer before emptying the first
-      pos=0; //initialize back the position
-      for (int i=0;i<GLOBAL_BUF;i=i+8) {
-        if (globalbuffer1[i]!=initBuff) {
-          testFile.print(globalbuffer2[i]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+1]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+2]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+3]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+4]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+5]); testFile.print("  ");
-          testFile.print(globalbuffer2[i+6]); testFile.print("  ");
-          testFile.println(globalbuffer2[i+7]);
-          globalbuffer2[i]=initBuff;
-        }
-      }
-    }
-    // print to the serial port too:
   }
+    // print to the serial port too:
   // if the file isn't open, pop up an error:
   else {
-    Serial.println("error opening testlog.txt");
+    Serial.println("error opening ");
+    Serial.println(str);
   }
-  testFile.close();*/
-  //Serial.println(timecheck);
+  testFile.close();
 
-  /*
+  if (sinceFile >= filePeriod) { //create a new file periodically to limit the weight on the ram
+    sinceFile = sinceFile - filePeriod; //decrement and adjust for latency
+    increment_str(&(fileIdx[0]),&(str[4])); //change the file name
+  }
+
+  //Serial.println("bonjour");
+  Serial.println(timecheck);
+/*
   digitalWrite(ledPin, HIGH);   // set the LED on
   delay(100);                  // wait for a second
   digitalWrite(ledPin, LOW);    // set the LED off
   delay(100);
   */
+  //if (timecheck>criticTime) Serial.print("tu pues mec !");
+
 }
 
 void setup_dma() {
@@ -221,12 +248,29 @@ void setup_adc() {
 
 }
 
-void d2_isr(void) {
-  if(debounce > 200){
-    d2_active = 1;
-    debounce = 0;
+void setup_files() {
+
+  for (int seti=0;seti<100;seti=seti+1) {
+    if (SD.exists(str)) {
+        increment_str(&(setupIdx[0]), &(str[1]));
+        Serial.println(seti);
     }
-    else{return;}
+  }
+
+  for (int setj=0;setj<nbFiles;setj=setj+1) {
+    for (int setk=48;setk<56;setk=setk+1) {
+      str[3]=setk;
+      File setupFile = SD.open(str, FILE_WRITE);
+      setupFile.close();
+    }
+    increment_str(&(fileIdx[0]), &(str[4])); //change the file name
+  }
+
+  fileIdx[0]=48;
+  fileIdx[1]=48;
+  str[4]=fileIdx[1];
+  str[5]=fileIdx[0];
+
 }
 
 void dma0_isr(void) {
@@ -237,32 +281,30 @@ void dma0_isr(void) {
     digitalWriteFast(4, LOW);
 }
 
-void callback(void) {/*
-  if ((pos + BUF_SIZE - 1) < GLOBAL_BUF) {
-    if (BUFF==false) {
-      for (int i=0;i<BUF_SIZE;i++) {
-        //globalbuffer1[pos+i]=adcbuffer[i];
-        Serial.print(adcbuffer[i]);
-      }
-    }
-    else {
-      for (int i=0;i<BUF_SIZE;i++) {
-        globalbuffer2[pos+i]=adcbuffer[i];
-      }
-    } pos = pos + BUF_SIZE;
-  }*/
+void increment_idx(int *idx) {
 
-  Serial.print(adcbuffer[0]); Serial.print("  ");
-  Serial.print(adcbuffer[1]); Serial.print("  ");
-  Serial.print(adcbuffer[2]); Serial.print("  ");
-  Serial.print(adcbuffer[3]); Serial.print("  ");
-  Serial.print(adcbuffer[4]); Serial.print("  ");
-  Serial.print(adcbuffer[5]); Serial.print("  ");
-  Serial.print(adcbuffer[6]); Serial.print("  ");
-  Serial.print(adcbuffer[7]); Serial.print("  ");
-  Serial.println(callbacktime);
+  *(idx)=*(idx)+1;
+  if (*(idx)>57) {
+    *(idx)=48;
+    *(idx+1)=*(idx+1)+1;
+  }
+
 }
 
-void fileManager(void) {
+void increment_str(int *idx, char* s) {
 
+  increment_idx(&(*(idx)));
+
+  for (int k=0; k<2; k=k+1) {
+    *(s+k)=*(idx+1-k);
+  }
+
+}
+
+void callback(void) {
+  if ((pos + BUF_SIZE - 1) < GLOBAL_BUF){
+    for (int i=0;i<BUF_SIZE;i++) {
+      globalbuffer1[pos+i]=adcbuffer[i];
+    }
+  } pos = pos + BUF_SIZE;
 }
